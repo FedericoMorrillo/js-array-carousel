320250f6e269f6195b85c19ac0e8d9a3a0e41ca5
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-11 11:58:08 +0100

    feat: frecce funzionanti senza ciclo infinito

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 1) definiamo un array con le immagini date;
 2) eliminiamo il markup statico e reinseriamo le immagini dinamicamente attraverso l' array e un ciclo;
-3) una volta inserite le immagini e riassegnamo la classe;
-4) sfruttiamo le classi per far modo che a schermo rimanga l' immagine selezionata;
-5) aggiungiamo una funzione alle frecce on click in modo che la classe che fa mostrare l' immagine sia riassegnata all'   immagine successiva o precedente;
+3) appendiamo le immagini al div e quest ultimo al suo contenitore;
+4) inseriamo una variabile che definisce la posizione di partenza delle slide;
+5) aggiungiamo una funzione alle frecce on click in modo che la classe che fa mostrare l' immagine sia riassegnata all' immagine successiva o precedente attraverso il contatore del ciclo;
--- js/main.js
+++ js/main.js
@@ -2,17 +2,32 @@
 
 // array di immagini
 const images = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
+
+//ci prendiamo dall' html il div items
 const items = document.querySelector(".items");
+
+//ci prendiamo dall' html il div prev e next
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+
+ //posizione di partenza del ciclo
+    let currentItem = 0;
 
 //inizio ciclo
 for(let i=0; i < images.length; i++){
+   
 
+//SLIDE***********************************************
     //creiamo il div per le slide
 const item = document.createElement('div');
 
 //gli assegniamo una classe
 item.classList.add('item');
 
+//assegniamo la classe active per far vedere l immagine
+if(currentItem ===i){
+    item.classList.add('active');
+}
 //creiamo l' elemento img
 const img = document.createElement('img');
 
@@ -24,4 +39,35 @@ item.append(img);
 
 //appendiamo le slide nel suo contenitore
 items.append(item);
+
+//SLIDE***************************************************
 }
+//selezioniamo tutte le slide del contenitore items
+const domItem = document.querySelectorAll('.item');
+
+console.log(domItem);
+
+
+//aggiungiamo eventi on click sulle frecce
+prev.addEventListener('click', function(){
+    console.log("prev");
+    if(currentItem > 0){
+        domItem[currentItem].classList.remove('active');
+
+        currentItem--;
+
+        domItem[currentItem].classList.add('active');
+    }
+})
+
+next.addEventListener('click', function(){
+    console.log(currentItem);
+    if(currentItem < domItem.length -1){
+        domItem[currentItem].classList.remove('active');
+
+        currentItem++;
+
+        domItem[currentItem].classList.add('active');
+    }
+   
+})
